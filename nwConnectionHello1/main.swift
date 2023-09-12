178187d5924d5b3b3822c67bf594a6ede2eb954b
wangbin@i-tek.cn <wangbin@i-tek.cn>
2023-09-12 15:21:14 +0800

    1

--- nwConnectionHello1/main.swift
+++ nwConnectionHello1/main.swift
@@ -6,6 +6,42 @@
 //
 
 import Foundation
+import Network
 
-print("Hello, World!")
+let msg="Hello,world!"
+let hostUDP: NWEndpoint.Host = "127.0.0.1"
+let portUDP: NWEndpoint.Port = 20131
+    
+var connection=NWConnection(host: hostUDP, port: portUDP, using: .udp)
+    
+connection.stateUpdateHandler = { (newState) in
+    print("This is stateUpdateHandler:")
+    switch (newState) {
+        case .ready:
+            print("State: Ready\n")
+            sendUDP(msg)
+        case .setup:
+            print("State: Setup\n")
+        case .cancelled:
+            print("State: Cancelled\n")
+        case .preparing:
+            print("State: Preparing\n")
+        default:
+            print("ERROR! State not defined!\n")
+    }
+}
+    
+connection.start(queue: .global())
+    
+func sendUDP(_ msg:String){
+    let contentToSend=msg.data(using: String.Encoding.utf8)
+    connection.send(content: contentToSend, completion: NWConnection.SendCompletion.contentProcessed({(NWError) in
+        if NWError==nil{
+            print("Data was sent to UDP")
+        }else{
+            print("ERROR! Error when data (Type: Data) sending. NWError: \n \(NWError!)")
+        }
+    }))
+}
+sendUDP(msg)
 
